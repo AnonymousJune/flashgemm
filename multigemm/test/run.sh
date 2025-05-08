3549363f96535afa2794da5af185e53eb82ec064
zjw <3207748139@qq.com>
2025-05-08 19:10:10 +0800

    run.sh

--- multigemm/test/run.sh
+++ multigemm/test/run.sh
@@ -2,4 +2,9 @@ export KMP_HW_SUBSET=1T
 export OMP_PROC_BIND=spread
 export OMP_PLACES=threads
 export OMP_NUM_THREADS=24 
-./a.out
+./test_f32.out
+./test_f32_3.out
+./test_bf16.out
+./test_bf16_3.out
+./test_int8.out
+./test_int8_3.out
